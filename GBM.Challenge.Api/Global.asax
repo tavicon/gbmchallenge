f8e1a4f4f6521cadedcbfbf9cad9b57b4310aa39
Gustavo Rivera Cabrera <grchsm11@hotmail.com>
2019-01-11 20:24:20 -0600

    Parte 1.

---
+++ GBM.Challenge.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GBM.Challenge.Api.WebApiApplication" Language="C#" %>
---
+++ GBM.Challenge.Tools.Data.Storage/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBM.Challenge.Tools.Data.Storage
+{
+    public class Class1
+    {
+    }
+}
